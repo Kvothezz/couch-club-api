7e60460c2b0ff2d5e66e029ef712b8a3a26a096f
Fábio Bezerra <bezerradeoliveirafabio@gmail.com>
2026-02-26 18:48:38 -0300

    feat: implement validation dtos for auth and users

---
+++ src/auth/dtos/loginDto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class LoginDto {
+    @IsEmail()
+    @ApiProperty({ example: 'test@gmail.com', description: 'User email' })
+    email: string;
+    @ApiProperty({ example: 'strongPassword123', description: 'User password' })
+    @IsNotEmpty()
+    password: string;
+}   
